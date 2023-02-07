1be388cb12b3174c47fbdf1ff561a877658b69f4
GeorgeDB <124641422+GeorgeDB@users.noreply.github.com>
2023-02-07 18:47:41 -0300

    Update main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,7 +1,6 @@
 ############################################
 # Action recognition app
 # George Dechichi Barbar
-# https://youtu.be/doDUihpj6ro
 ############################################
 
 import cv2
